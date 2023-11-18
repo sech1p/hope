c3371bec7c780bf9fe08a664d28981f9ee0d4f4b
sech1p <sech1p@disroot.org>
2023-11-18 17:32:17 +0100

    🔧 Events handler

--- hope/src/Hope.ts
+++ hope/src/Hope.ts
@@ -3,12 +3,27 @@ import fs from "node:fs";
 import path from "node:path";
 import Config from "./Config";
 import startServer from "./web/HopeServer";
+import glob from "glob-promise";
 
 const bot = Eris(Config.Token);
 
-bot.on("ready", () => {
-    console.log("? Bot is ready!");
-});
+const loadEvents = async (bot) => {
+    console.log(`? Loading events...`)
+    await glob(`dist/events/*.js`).then(async (eventsFiles) => {
+        for (const eventFile of eventsFiles) {
+            try {
+                const { default: event } = await import(path.join(process.cwd(), eventFile));
+                if (typeof event.run === "function") {
+                    bot.on(event.name, event.run.bind(null, bot));
+                } else {
+                    console.error("? Invalid event file, run function is missing");
+                };
+            } catch (exception) {
+                console.error(`? Failed to load events: ${exception}`);
+            }
+        }
+    });
+};
 
 bot.on("messageCreate", (message) => {
     if (message.content === "!ping") {
@@ -17,6 +32,7 @@ bot.on("messageCreate", (message) => {
 });
 
 const start = async () => {
+    await loadEvents(bot);
     await Promise.all([startServer(), bot.connect()]);
 }
 start();
---
+++ hope/src/events/ready.ts
@@ -0,0 +1,6 @@
+export default {
+    name: "ready",
+    run: (bot) => {
+        console.log("? Bot is ready!");
+    },
+};
