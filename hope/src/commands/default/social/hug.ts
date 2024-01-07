ee518b795e9e3efa2e4a4c85fa835fd5fcf4073d
sech1p <sech1p@disroot.org>
2024-01-07 23:29:20 +0100

    ➕ Add hug command

---
+++ hope/src/commands/default/social/hug.ts
@@ -0,0 +1,22 @@
+import giphy from "../../../Hope";
+import Embed from "../../../utils/Embed";
+
+export default {
+    name: "hug",
+    description: "Hug someone!",
+    execute: async (bot, message, args) => {
+        const user = args[0];
+        giphy.giphy.search("anime hug", (exception, result) => {
+            if (exception) console.error(exception);
+
+            const randomNumber = Math.floor(Math.random() * 10);
+            const embed = new Embed.EmbedBuilder({
+                image: {
+                    url: result.data[randomNumber].images.original.url,
+                },
+            });
+            bot.createMessage(message.channel.id, `${user}, you are hugged!`);
+            bot.createMessage(message.channel.id, { embed: embed.build() });
+        });
+    },
+};
