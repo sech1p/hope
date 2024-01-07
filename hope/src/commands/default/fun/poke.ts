9cebae819223007781d370c11e7f6d229900bf2d
sech1p <sech1p@disroot.org>
2024-01-07 22:04:43 +0100

    🗑️ Remove social commands from fun category

--- hope/src/commands/default/fun/poke.ts
+++
@@ -1,8 +0,0 @@
-export default {
-    name: "poke",
-    description: "Poke someone!",
-    execute: async (bot, message, args) => {
-        const user = args[0];
-        bot.createMessage(message.channel.id, `Poked ${user}!`);
-    },
-};
