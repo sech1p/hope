decbd08da111cb4a60ac5f85ebeb53174cd249b9
sech1p <sech1p@disroot.org>
2024-01-08 00:03:50 +0100

    ➕ Add 8ball command

---
+++ hope/src/commands/default/fun/8ball.ts
@@ -0,0 +1,27 @@
+import giphy from "../../../Hope";
+import Embed from "../../../utils/Embed";
+
+export default {
+    name: "8ball",
+    description: "Ask magic ball about everything",
+    execute: async (bot, message, args) => {
+        const responses = [
+            "Yes!",
+            "Nope",
+            "No",
+            "Maybe...",
+            "NEVER",
+        ];
+        const randomNumber = Math.floor(Math.random() * responses.length);
+        const embed = new Embed.EmbedBuilder({
+            title: "🎱 Magic 8ball",
+            description: "Thinking...",
+        });
+        const newEmbed = new Embed.EmbedBuilder({
+            title: "🎱 Magic 8ball",
+            description: `${responses[randomNumber]}`,
+        });
+        const messageSent = await bot.createMessage(message.channel.id, { embed: embed.build() });
+        messageSent.edit({ embed: newEmbed.build() });
+    },
+};
