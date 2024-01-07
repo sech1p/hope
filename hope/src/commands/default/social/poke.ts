bac0980f5f48e33967372513b8f5daaf23f6c3c7
sech1p <sech1p@disroot.org>
2024-01-07 23:19:46 +0100

    ➕ Add poke command

--- hope/src/commands/default/social/poke.ts
+++ hope/src/commands/default/social/poke.ts
@@ -1,8 +1,21 @@
+import giphy from "../../../Hope";
+import Embed from "../../../utils/Embed";
+
 export default {
     name: "poke",
     description: "Poke someone!",
     execute: async (bot, message, args) => {
         const user = args[0];
-        bot.createMessage(message.channel.id, `Poked ${user}!`);
+        giphy.giphy.search("anime poke", (exception, result) => {
+            if (exception) console.error(exception);
+
+            const embed = new Embed.EmbedBuilder({
+                image: {
+                    url: result.data[0].images.original.url,
+                },
+            });
+            bot.createMessage(message.channel.id, `${user}, you are poked!`);
+            bot.createMessage(message.channel.id, { embed: embed.build() });
+        });
     },
 };
