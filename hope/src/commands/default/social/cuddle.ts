72cb80071dfdaa6337bcea2555016a9d05ead193
sech1p <sech1p@disroot.org>
2024-01-08 10:54:30 +0100

    ➕ Add cuddle command

---
+++ hope/src/commands/default/social/cuddle.ts
@@ -0,0 +1,22 @@
+import giphy from "../../../Hope";
+import Embed from "../../../utils/Embed";
+
+export default {
+    name: "cuddle",
+    description: "Cuddle someone!",
+    execute: async (bot, message, args) => {
+        const user = args[0];
+        giphy.giphy.search("anime cuddle", (exception, result) => {
+            if (exception) console.error(exception);
+
+            const randomNumber = Math.floor(Math.random() * 10);
+            const embed = new Embed.EmbedBuilder({
+                image: {
+                    url: result.data[randomNumber].images.original.url,
+                },
+            });
+            bot.createMessage(message.channel.id, `${user}, you are cuddled!`);
+            bot.createMessage(message.channel.id, { embed: embed.build() });
+        });
+    },
+};
