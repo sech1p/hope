5cda9a5ebe6bd131ae1a148abd52fbf7a7110c9f
sech1p <sech1p@disroot.org>
2024-01-09 19:34:12 +0100

    ➕ Add tickle command

---
+++ hope/src/commands/default/social/tickle.ts
@@ -0,0 +1,29 @@
+import giphy from "../../../Hope";
+import Embed from "../../../utils/Embed";
+
+export default {
+    name: "tickle",
+    description: "Poke someone!",
+    execute: async (bot, message, args) => {
+        const tickleGifs = [
+            "FDPJjtvBkIRYA",
+            "2QHLYZFJgjsFq",
+            "FNyJEVi76JgzK",
+        ]
+        const MAX = tickleGifs.length - 1;
+        const MIN = 0;
+        const user = args[0];
+        const randomNumber = Math.floor(Math.random() * (MAX - MIN + 1) + MAX); // Generate random number between 0-2 indexes
+        giphy.giphy.id(tickleGifs[randomNumber], (exception, result) => {
+            if (exception) console.error(exception);
+
+            const embed = new Embed.EmbedBuilder({
+                image: {
+                    url: result.data[0].images.original.url,
+                },
+            });
+            bot.createMessage(message.channel.id, `${user}, you have been tickled!`);
+            bot.createMessage(message.channel.id, { embed: embed.build() });
+        });
+    },
+};
