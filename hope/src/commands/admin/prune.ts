9646dabfd378e231f58a7a83acb418e388c81aca
sech1p <sech1p@disroot.org>
2024-01-09 21:00:06 +0100

    ➕ Add prune command

---
+++ hope/src/commands/admin/prune.ts
@@ -0,0 +1,44 @@
+import { DiscordRESTError } from "eris";
+import Embed from "../../utils/Embed";
+import Colors from "../../utils/Colors";
+
+export default {
+    name: "prune",
+    description: "🗑 Prune message(s) from Guild",
+    execute: async (bot, message, args) => {
+        const errorEmbed = (message) => {
+            const embedFail = new Embed.EmbedBuilder({
+                title: "❌ Sorry!",
+                description: message,
+                color: Colors.Red,
+            });
+            return embedFail.build();
+        };
+
+        const permission = message.channel.permissionsOf(bot.user.id).json;
+        if (!permission.manageMessages) {
+            return bot.createMessage(message.channel.id, { embed: errorEmbed("I don't have a permission") });
+        }
+
+        let messageCount = args[0];
+        if (isNaN(messageCount)) {
+            return bot.createMessage(message.channel.id, { embed: errorEmbed(`\`${messageCount}\` is not a valid number`) });
+        }
+
+        messageCount = parseInt(messageCount);
+        if (!messageCount || messageCount < 1) {
+            return bot.createMessage(message.channel.id, { embed: errorEmbed("Please insert a number") });
+        }
+        if (messageCount > 100) {
+            return bot.createMessage(message.channel.id, { embed: errorEmbed("Too many messages") });
+        }
+
+        await message.delete();
+
+        message.channel.purge({ limit: messageCount })
+            .then(amount => {
+            })
+            .catch(exception => {
+            });
+    },
+};
