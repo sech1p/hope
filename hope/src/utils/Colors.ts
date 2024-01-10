c5a9306814c404b74fc6dd8443b49e22899ac7bf
sech1p <sech1p@disroot.org>
2024-01-10 10:57:44 +0100

    ➕ Add more colors

--- hope/src/utils/Colors.ts
+++ hope/src/utils/Colors.ts
@@ -1,7 +1,19 @@
 const Green = 0x00FF00;
 const Red = 0xFF0000;
+const Purple = 0x8E44AD;
+const Blue = 0x3498DB;
+const Yellow = 0x3498DB;
+const White = 0x3498DB;
+const Silver = 0x99A3A4;
+const Gray = 0x616A6B;
 
 export default {
     Green,
     Red,
+    Purple,
+    Blue,
+    Yellow,
+    White,
+    Silver,
+    Gray,
 };
