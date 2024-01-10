5cce4aa108f5642f102a2b4f2a4943f068117e6b
sech1p <sech1p@disroot.org>
2024-01-10 11:10:22 +0100

    🗑 Remove WSclose event

--- hope/src/events/WSclose.ts
+++
@@ -1,8 +0,0 @@
-import Hope from "../Hope";
-
-export default {
-    name: "ready",
-    run: (bot) => {
-        Hope.logError("✖ WebSocket is closed (check your internet connection)");
-    },
-};
