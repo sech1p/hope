a3ef7af9c9df6ab2477e2c7903880eb69cc6ae59
sech1p <sech1p@disroot.org>
2024-01-14 18:54:56 +0100

    ➕ Set bot status

--- hope/src/events/ready.ts
+++ hope/src/events/ready.ts
@@ -5,5 +5,10 @@ export default {
     name: "ready",
     run: (bot: Eris.Client) => {
         Hope.log("🌸 Bot is ready!");
+        bot.editStatus("dnd", {
+            name: "h!help",
+            type: 0,
+            url: "https://sech1p.ovh",
+        });
     },
 };
