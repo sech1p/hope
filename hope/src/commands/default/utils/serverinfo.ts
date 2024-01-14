566bebdb587cd05cdd9de80fb5c80845f7547983
sech1p <sech1p@disroot.org>
2024-01-14 18:08:57 +0100

    ➕ Add serverinfo command

---
+++ hope/src/commands/default/utils/serverinfo.ts
@@ -0,0 +1,44 @@
+import Eris from "eris";
+import Embed from "../../../utils/Embed";
+import Hope from "../../../Hope";
+import moment from "moment";
+import Colors from "../../../utils/Colors";
+
+export default {
+    name: "serverinfo",
+    description: "📝 Get informations about Guild",
+    execute: (bot: Eris.Client, message: Eris.Message, args: string[]) => {
+        const guild = bot.guilds.get(message.guildID);
+        const embed = new Embed.EmbedBuilder({
+            author: {
+                name: guild.name,
+                icon_url: guild.iconURL,
+            },
+            fields: [
+                {
+                    name: "🎂 Guild cake day",
+                    value: moment(guild.createdAt).format("LLLL"),
+                },
+                {
+                    name: "Guild owner",
+                    value: `<@${guild.ownerID}>`,
+                },
+                {
+                    name: "Member count",
+                    value: String(guild.memberCount),
+                },
+                {
+                    name: "Locale",
+                    value: guild.preferredLocale,
+                },
+                {
+                    name: "NSFW?",
+                    value: guild.nsfw ? "🔞": "No",
+                },
+            ],
+            color: Colors.RANDOM[Math.floor(Math.random() * Colors.RANDOM.length)],
+            footer: Hope.footer(message),
+        });
+        bot.createMessage(message.channel.id, { embed: embed.build() });
+    },
+};
