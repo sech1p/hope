4582bbf87f1e16bc2df86aa2b18bd4bf6f88514b
sech1p <sech1p@disroot.org>
2024-01-16 15:28:31 +0100

    ➕ Social: Add pat command

---
+++ hope/src/commands/default/social/pat.ts
@@ -0,0 +1,31 @@
+import Eris from "eris";
+import Config from "../../../Config";
+import Hope from "../../../Hope";
+import Colors from "../../../utils/Colors";
+import Embed from "../../../utils/Embed";
+
+export default {
+    name: "pat",
+    description: "(づ ˘͈ ᵕ ˘͈ )づ Pat someone!",
+    category: "default/Social",
+    usage: `${Config.Prefix}pat [user]`,
+    exampleUsage: `${Config.Prefix}pat @user`,
+    subcommands: "N/A",
+    execute: async (bot: Eris.Client, message: Eris.Message, args: string[]) => {
+        const user = args[0];
+        Hope.giphy.search("anime pat", (exception, result) => {
+            if (exception) console.error(exception);
+
+            const randomNumber = Math.floor(Math.random() * 10);
+            const embed = new Embed.EmbedBuilder({
+                image: {
+                    url: result.data[randomNumber].images.original.url,
+                },
+                color: Colors.Pink,
+                footer: Hope.footer(message),
+            });
+            bot.createMessage(message.channel.id, `${user}, you have been headpated!`);
+            bot.createMessage(message.channel.id, { embed: embed.build() });
+        });
+    },
+};
