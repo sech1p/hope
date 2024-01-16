4a9cc20667caadd33d7ec4cd30e564815325e303
sech1p <sech1p@disroot.org>
2024-01-16 15:13:27 +0100

    ➕ Add eslint and babel

---
+++ hope/.eslintrc.js
@@ -0,0 +1,40 @@
+// Inspired by https://gist.github.com/adrianhall/70d63d225e536b4563b2
+
+let codes = {
+    OFF: 0,
+    WARN: 1,
+    ERROR: 2,
+};
+
+module.exports = exports = {
+    "env": {
+        "es2021": true,
+    },
+    "parser": "@babel/eslint-parser",
+    "parserOptions": {
+        "sourceType": "module",
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-extra-parens": codes.ERROR,
+        "no-unexpected-multiline": codes.ERROR,
+        "no-undef": codes.OFF,
+        "no-unused-vars": codes.OFF,
+        "no-case-declarations": codes.OFF,
+        "valid-jsdoc": [ codes.ERROR, {
+            "requireReturn": true,
+            "requireReturnDescription": true,
+            "requireParamDescription": true,
+            "prefer": {
+                "return": "returns",
+            },
+        }],
+    },
+    "settings": {
+        "import/resolver": {
+            "node": {
+                "paths": ["src"],
+            },
+        },
+    },
+};
